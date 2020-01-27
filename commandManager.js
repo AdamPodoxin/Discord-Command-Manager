f1731f4d7080be2075aacd2dcc467c7685cf1463
Adam Pod <adam.podoxin@gmail.com>
2020-01-27 14:22:46 -0800

    Create Command class

---
+++ commandManager.js
@@ -0,0 +1,19 @@
+//----------------COMMAND MANAGER----------------
+
+class Command {
+    constructor(name, requirePermissions, action) {
+        this.name = name;
+        this.requirePermissions = requirePermissions;
+        this.action = action;
+    }
+}
+
+//----------------COMMAND MANAGER----------------
+
+
+
+//-------------------EXAMPLE---------------------
+
+
+
+//-------------------EXAMPLE---------------------
