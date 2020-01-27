15790cb9a834e4f6149210ad323f59b8c8476d66
Adam Pod <adam.podoxin@gmail.com>
2020-01-27 15:45:06 -0800

    Create onMessage method in CommandManager

--- commandManager.js
+++ commandManager.js
@@ -12,7 +12,11 @@ class CommandManager {
     constructor() {
         this.commands = [];
         this.specialPermissionRoles = [];
-        this.prefix = "!";
+        this.prefix = '!';
+    }
+
+    getMemberByUserId(userId, guild) {
+        return guild.members.get(userId);
     }
 
     setPrefix(prefix) {
@@ -27,7 +31,41 @@ class CommandManager {
         this.commands.push(new Command(name, requirePermissions, action));
     }
 
+    onMessage(message) {
+        if (message.author.bot)
+            return;
 
+        //Checks if the message is a command
+        if (message.content.charAt(0) == this.prefix) {
+            //Splits the message into seperate parts at spaces
+            const args = message.content.substring(1, message.content.length).split(" ");
+
+            //Loops through each command to see which one is called
+            for (var i = 0; i < this.commands.length; i++) {
+                const command = this.commands[i];
+
+                if (command.name == args[0]) {
+                    if (command.requirePermissions) {
+                        //Checks if the author of the message has required permissions for this command
+                        this.specialPermissionRoles.forEach(role => {
+                            if (this.getMemberByUserId(message.author.id, message.guild).roles.some(r => r.name == role)) {
+                                command.action(message);
+                            } else {
+                                message.reply("you do not have the required permissions for this command.");
+                            }
+                        });
+                    } else {
+                        command.action(message);
+                    }
+
+                    return;
+                }
+            }
+
+            //If desired command was not found
+            message.reply("this command was not found.");
+        }
+    }
 }
 
 //----------------COMMAND MANAGER----------------
